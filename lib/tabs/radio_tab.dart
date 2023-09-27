e090c1d4e61f8258eefe1db9a71269d8ca37dc9b
HP-BANG <mennatallah.emad265@gmail.com>
2023-09-27 18:04:40 +0300

    create radio screen and load radio image

--- lib/tabs/radio_tab.dart
+++ lib/tabs/radio_tab.dart
@@ -6,8 +6,15 @@ class RadioTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blue,
+    return Column(
+      children: [
+        Image.asset('assets/images/radio_image.png',width: 412,height: 222,),
+        SizedBox(height: 25,),
+        Text('إذاعة القرأن الكريم',style: Theme.of(context).textTheme.bodyLarge,),
+        SizedBox(height: 80,),
+        Image.asset('assets/images/Group 5.png')
+      ],
+
     );
   }
 }
